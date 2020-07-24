eae6c5f3da87303db5f924510be3615aadeffb43
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2020-07-24 13:41:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
   let image = 
   "https://www.pngitem.com/pimgs/m/233-2336335_transparent-aang-png-avatar-aang-png-download.png";
   let title1 = "HTML Full Course - Build a Website Tutorial";
+  let title2 = "CSS Tutorial - Zero to Hero";
   let desc1 = "Learn the basics of HTML5 and web development in this awesome course for beginners.";
+  let desc2 = "Learn CSS in this full course for beginners. CSS, or Cascading Style Sheet, is responsible for the styling and looks of a website. ";
   return (
     <div className="App">
       <InfoStudent 
@@ -27,9 +29,9 @@ function App() {
         <div className="item-2">
           <CardCourses 
             url={image}
-            title={title1}
-            desc={desc1}
-            link={"https://www.youtube.com/embed/pQN-pnXPaVg"}
+            title={title2}
+            desc={desc2}
+            link={"https://www.youtube.com/embed/1Rs2ND1ryYc"}
           />
         </div>
       </div>
